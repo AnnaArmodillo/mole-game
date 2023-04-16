2ee758341f6d06ef3359a98becab117660a9447b
Anna Denisova <abachledacurus@gmail.com>
2023-04-16 21:27:34 +0700

    Новый билд с изменениями и исправлениями

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
       ],
     },
   ],
-  // { basename: '/mole-game' },
+  { basename: '/mole-game' },
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
